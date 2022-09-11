2a25ace7eee579753648b39b7884286df4d0fe45
naanko <nbarko04@gmail.com>
2022-09-11 16:33:15 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,22 @@
+#PYTHON ASSIGNMENT
+#Differentaite between Mutable and Immutables Data types in Python
+
+#Mutable is a type of variable that can be changed.
+#A mutable object is an object whose state/value can be modified after it is created. 
+#Some of the mutable data types in python are list, dictionary, set and user-defined classes.
+
+#Immutables are the objects whose state/content cannot be changed once the object is created.
+#  An object can be immutable for various reasons,
+#for example:
+#     • To improve performance (no planning for the object's future changes)
+#     • To reduce memory use (make object references instead of cloning the whole object)
+#     • Thread-safety (multiple threads can reference the same object without interfering with one other)
+# Strings and Numbers are Immutable.
+#Some of the immutable data types are integers, float, decimal, boolean, strings, tuple and rage.
+#Once they are initialized, their values cannot be changed.
+
+#For example, Comparing the tuple(immutable) and list(mutable) data types. 
+#We can define a list using 'square brackets' [] like this: numbers = [1, 2, 3].
+#To define a tuple, we just need to replace the brackets with parentheses () like this: (1, 2, 3).
+#From both data types, we can access elements by index and we can iterate over them.
+#The main difference is that tuple cannot be changed once it is defined.
